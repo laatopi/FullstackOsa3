eaab86638d050b76d8bb36eaeab54c301f639a1f
Topi Laanti <laantop@gmail.com>
2018-01-29 21:36:17 +0200

    intiate app

---
+++ index.js
@@ -0,0 +1,97 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+morgan.token('data', function (request, respond) {
+    return JSON.stringify(request.body)
+})
+
+
+app.use(morgan('tiny'))
+app.use(morgan(':data'))
+app.use(bodyParser.json())
+app.use(cors())
+
+let persons = [
+    {
+        name: 'Arto Hellas',
+        number: '040-123456',
+        id: 1
+    },
+    {
+        name: 'Martti Tienari',
+        number: '040-123456',
+        id: 2
+    },
+    {
+        name: 'Arto Järvinen',
+        number: '040-123456',
+        id: 3
+    },
+    {
+        name: 'Lea Kutvonen',
+        number: '040-123456',
+        id: 4
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (body.name === undefined || body.number === undefined) {
+        return response.status(400).json({ error: 'name or number missing' })
+    }
+    const nimi = persons.find(person => person.name === body.name)
+    if (nimi !== undefined) {
+        return response.status(400).json({ error: 'name must be unique' })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * Math.floor(1000))
+    }
+    console.log(person.id)
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    console.log('haloo')
+    response.status(204).end()
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(`Puhelinluettelossa ${persons.length} henkilön tiedot \n
+    <p>${date}</p>`)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})
+
+app.listen(port)
+console.log(`Server Running on port ${port}`)
